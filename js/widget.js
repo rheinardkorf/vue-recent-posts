ad8db1015ff60ffb422e52062e1a5c69c76ce287
Rheinard Korf <rheinard@thekorfs.com>
2016-12-07 15:21:15 +1100

    Now plays nice with Customizer.

--- js/widget.js
+++ js/widget.js
@@ -1,3 +1,6 @@
+/**
+ * Register the vue-recent-posts component.
+ */
 Vue.component( 'vue-recent-posts', {
 	props: ['posts', 'showDate'],
 	template: '<ul><li v-for="post in posts"><a :href="post.link">{{ post.title.rendered }}</a> ' +
@@ -10,10 +13,12 @@ Vue.component( 'vue-recent-posts', {
 	}
 } );
 
-var widgets = document.querySelectorAll( '.widget_vue_recent_posts_widget' );
 
-widgets.forEach( function( item, i ) {
-
+/**
+ * Setup an individual widget with a Vue instance.
+ * @param item
+ */
+function setupWidget( item ) {
 	var widgetID = item.getAttribute( 'id' );
 	var element = item.querySelector( '.vue-widget' );
 	var postCount = element.getAttribute( 'data-number' ) || 5;
@@ -43,6 +48,58 @@ widgets.forEach( function( item, i ) {
 				}
 			}
 			request.send();
+		},
+		destroyed: function() {
+			console.log('I am destroyed.');
 		}
 	} );
-} );
+}
+
+
+/**
+ * Vanilla version of $(document).ready()
+ * @param fn
+ */
+function ready(fn) {
+	if (document.readyState != 'loading'){
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+/**
+ * Once the doc is ready, lets roll.
+ */
+ready(function(){
+
+	/**
+	 * Grab all the Vue Recent Posts widgets.
+	 * @type {NodeList}
+	 */
+	var widgets = document.querySelectorAll( '.widget_vue_recent_posts_widget' );
+
+	/**
+	 * Setup all the widgets with Vue instances
+	 */
+	widgets.forEach( function( item ) {
+		setupWidget( item );
+	} );
+
+
+	/**
+	 * Play nice with Customizer selective refresh.
+	 */
+	if ( 'undefined' === typeof wp || ! wp.customize || ! wp.customize.selectiveRefresh ) {
+		return;
+	}
+
+	/**
+	 * Re-init the widget upon partial refresh.
+	 */
+	wp.customize.selectiveRefresh.bind( 'partial-content-rendered', function( placement ) {
+		if ( placement.container ) {
+			setupWidget( placement.container[0] );
+		}
+	} );
+});
--- vue-recent-posts.php
+++ vue-recent-posts.php
@@ -9,12 +9,17 @@ Author URI: http://rheinardkorf.com
 License: GPL2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: vue_recent_posts
-Domain Path: /languages
+Domain Path: .../
 */
 
-// Creating the widget
+/**
+ * Class Vue_Recent_Posts_Widget
+ */
 class Vue_Recent_Posts_Widget extends WP_Widget {
 
+	/**
+	 * Vue_Recent_Posts_Widget constructor.
+	 */
 	function __construct() {
 		parent::__construct(
 			// Base ID of your widget
@@ -25,13 +30,23 @@ class Vue_Recent_Posts_Widget extends WP_Widget {
 
 			// Widget options
 			array(
-				'description' => __( 'Vue driven recent posts.', 'vue_recent_posts' ),
-				//'customize_selective_refresh' => true,
+				'description'                 => __( 'Vue driven recent posts.', 'vue_recent_posts' ),
+				'customize_selective_refresh' => true,
 			)
 		);
+
+		// Enqueue style if widget is active (appears in a sidebar) or if in Customizer preview.
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		}
 	}
 
-
+	/**
+	 * Output the HTML that Vue will use to render the component.
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance The Widget instance.
+	 */
 	public function widget( $args, $instance ) {
 		$title = apply_filters( 'widget_title', $instance['title'] );
 		//// before and after widget arguments are defined by themes
@@ -41,42 +56,68 @@ class Vue_Recent_Posts_Widget extends WP_Widget {
 		echo $args['after_widget'];
 	}
 
-	// Widget Backend
+	/**
+	 * Output the widget backend UI.
+	 *
+	 * @param array $instance The Widget instance.
+	 *
+	 * @return void
+	 */
 	public function form( $instance ) {
 		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
 		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
 		$show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
 		?>
 		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
+		</p>
 
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
-			<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
+		<p><label
+				for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+			<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>"
+			       name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1"
+			       value="<?php echo $number; ?>" size="3"/></p>
 
-		<p><input class="checkbox" type="checkbox"<?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
-			<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
+		<p><input class="checkbox" type="checkbox"<?php checked( $show_date ); ?>
+		          id="<?php echo $this->get_field_id( 'show_date' ); ?>"
+		          name="<?php echo $this->get_field_name( 'show_date' ); ?>"/>
+			<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label>
+		</p>
 		<?php
 	}
 
+	/**
+	 * Update the widget instance.
+	 *
+	 * @param array $new_instance The new instance.
+	 * @param array $old_instance The old instance.
+	 *
+	 * @return array
+	 */
 	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		$instance['number'] = (int) $new_instance['number'];
+		$instance              = $old_instance;
+		$instance['title']     = sanitize_text_field( $new_instance['title'] );
+		$instance['number']    = (int) $new_instance['number'];
 		$instance['show_date'] = isset( $new_instance['show_date'] ) ? (bool) $new_instance['show_date'] : false;
+
 		return $instance;
 	}
-} // Class Vue_Recent_Posts_Widget ends here
 
-// Register and load the widget
+	/**
+	 * Load VueJS and the Vue component setup.
+	 */
+	function enqueue_scripts() {
+		wp_enqueue_script( 'vuejs', plugins_url( '/js/vue.min.js', __FILE__ ), array(), false, false );
+		wp_enqueue_script( 'vue_recent_posts', plugins_url( '/js/widget.js', __FILE__ ), array(), false, true );
+	}
+}
+
+/**
+ * Register and load the widget.
+ */
 function vue_recent_posts_load_widget() {
 	register_widget( 'Vue_Recent_Posts_Widget' );
 }
 
 add_action( 'widgets_init', 'vue_recent_posts_load_widget' );
-
-function vue_recent_posts_enqueue_script() {
-	wp_enqueue_script( 'vuejs', plugins_url( '/js/vue.min.js', __FILE__ ), array(), false, false );
-	wp_enqueue_script( 'vue_recent_posts', plugins_url( '/js/widget.js', __FILE__ ), array(), false, true );
-}
-
-add_action( 'wp_enqueue_scripts', 'vue_recent_posts_enqueue_script' );
